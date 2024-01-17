53349ababb3e1c52020d4b3ef87386673b57f1cf
gabriel <soutogabriel04@gmail.com>
2024-01-17 17:59:19 -0300

    feat: Pessoa candidata class added

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,8 +1,35 @@
 package com.betrybe.sistemadevotacao;
 
 /**
- * Pessoa candidata class.
+ * Classe abstrata PessoaCandidata.
  */
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
 
+  public int numero;
+  public int votos;
+
+  /**
+   * Classe abstrata PessoaCandidata.
+   */
+  public PessoaCandidata(String nome, int numero) {
+    this.numero = numero;
+    this.setNome(nome);
+    this.votos = 0;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public void receberVoto() {
+    this.votos += 1;
+  }
 }
