7532b46d135b97ba54d2ac3f653a87690f84481f
gabriel <soutogabriel04@gmail.com>
2024-01-17 18:08:29 -0300

    feat: Pessoa Eleitora class added

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -3,6 +3,20 @@ package com.betrybe.sistemadevotacao;
 /**
  * Javadoc comment for the PessoaCandidata class.
  */
-public class PessoaEleitora {
+public class PessoaEleitora extends Pessoa {
 
+  String cpf;
+
+  public PessoaEleitora(String nome, String cpf) {
+    this.cpf = cpf;
+    this.setNome(nome);
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
